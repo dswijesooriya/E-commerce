7b94d3809a5d628eef9dde45352157dae13eb8fc
D.S Wijesooriya <ict23949@fot.sjp.ac.lk>
2026-09-06 16:28:04 +0530

    Fix file name casing for Vercel (case-sensitive Linux)

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import { ShopContext } from './context/Shopcontext.jsx'
+import { ShopContext } from './context/ShopContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
